50e47420c4f63cedf9c10e083892ec990eb53b8c
brecec <brendaramirez@brendas-mbp.lan>
2023-06-25 13:44:35 -0500

    added prompts

--- index.js
+++ index.js
@@ -3,3 +3,37 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 
 // user prompts
+const questions = [
+    {
+        type: 'input',
+        message: 'Enter 3 text characters',
+        name: 'text',  
+    },
+    {
+        type: 'input',
+        message: 'Enter color for text',
+        name: 'textcolor',  
+    },
+    {
+        type: 'List',
+        message: 'Select logo shape',
+        name: 'shape', 
+        choices: ['circle', 'square', 'triangle','rectable'],
+    },
+    {
+        type: 'input',
+        message: 'Enter shape color',
+        name: 'shapecolor',  
+    },    
+];
+
+// call function to initialize app
+function init(){
+    inquirer.prompt(questions)
+        .then((answers) =>{
+            const createLogo = logoMaker(answers);
+            writeToFile("example.svg",createLogo);
+        })
+}
+
+init();
